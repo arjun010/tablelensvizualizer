904164ad5e40ad512439c9362f2c7812e10845f1
apc@apc.kg <apc@apc.kg@d5b0f64c-b8ad-11dd-b55b-2da8d274f894>
2008-11-22 17:37:30 +0000

    Начинаем парсить CSV

--- trunk/TableData.pas
+++ trunk/TableData.pas
@@ -3,11 +3,18 @@ unit TableData;
 interface
 type TTableData=class
   public
-    procedure SetByXY(x, y:Integer; Value:String );
+    procedure SetByXY(x, y:Integer; Value:String);
   private
-  
+
 end;
 implementation
+
+{ TTableData }
+
+procedure TTableData.SetByXY(x, y: Integer; Value: String);
+begin
+
+end;
 
 end.
  
