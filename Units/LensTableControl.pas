54daeca926a3394079d5f362c0f65c543ea5faa8
apc <apc@apc.kg>
2008-11-29 09:26:57 +0000

    Очищается фон и рисуются разделители колонок

--- Units/LensTableControl.pas
+++ Units/LensTableControl.pas
@@ -19,6 +19,7 @@ type TLensTableControl=class
     procedure PaintColumn(Box: TPaintBox; ColNo: TColIndex);
     procedure OnHeaderResize(Sender: TObject);
     procedure OnHeaderSectionResize(Header: THeaderControl; Section :THeaderSection);
+    procedure PaintGradientColumn(Box: TPaintBox; ColNo: TColIndex);
 end;
 
 implementation
@@ -67,16 +68,34 @@ if Header.Sections.Count>0 then
 end;
 
 procedure TLensTableControl.PaintColumn;
+begin
+// restore BG
+Box.Canvas.Pen.Color:=(Box.Parent as TPanel).Color;
+Box.Canvas.Brush.Color:=Box.Canvas.Pen.Color;
+Box.Canvas.Rectangle(Header.Sections[ColNo].Left, 0, Header.Sections[ColNo].Left+Header.Sections[ColNo].Width, Box.Height);
+
+// draw separator line
+Box.Canvas.Pen.Color:=clInactiveCaption;
+Box.Canvas.Brush.Color:=Box.Canvas.Pen.Color;
+Box.Canvas.MoveTo(Header.Sections[ColNo].Left+Header.Sections[ColNo].Width-1, 0);
+Box.Canvas.LineTo(Header.Sections[ColNo].Left+Header.Sections[ColNo].Width-1, Box.Height);
+
+// draw bars
+PaintGradientColumn(Box, ColNo);
+end;
+
+procedure TLensTableControl.PaintGradientColumn;
 var
   BarLeftX, BarRightX: word;
   BarTopY, BarBottomY: word;
   RowNo: TRowIndex;
   Cell:PDataCell;
+  BarHeight: Double;
 begin
-BarLeftX:=Header.Sections[ColNo].Left;
-
 Box.Canvas.Pen.Color:=clBlue;
 Box.Canvas.Brush.Color:=clBlue;
+
+BarLeftX:=Header.Sections[ColNo].Left;
 
 for RowNo:=0 to TableData.getRowCount-1 do
   begin
@@ -85,11 +104,10 @@ for RowNo:=0 to TableData.getRowCount-1 do
   BarTopY:=Round(RowNo*(PaintBox.Height/TableData.getRowCount));
   BarBottomY:=Round((RowNo+1)*(PaintBox.Height/TableData.getRowCount));
 
-  BarRightX:=BarLeftX+Round(Header.Sections[ColNo].Width * Cell.VisualValue);
+  BarRightX:=BarLeftX+Round((Header.Sections[ColNo].Width-1) * Cell.VisualValue);
 
   Box.Canvas.Rectangle(BarLeftX, BarTopY, BarRightX, BarBottomY);
   end;
-
 end;
 
 procedure TLensTableControl.PrepareLensTable;
